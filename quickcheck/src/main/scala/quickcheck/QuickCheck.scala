7602eca81acfa4e96168602613203233cfa66311
Albert <albert@albertmac.local>
2013-11-08 00:10:50 +0000

    Some more properties added although still 3, 4 red.

--- quickcheck/src/main/scala/quickcheck/QuickCheck.scala
+++ quickcheck/src/main/scala/quickcheck/QuickCheck.scala
@@ -14,8 +14,18 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
     findMin(h) == a
   }
 
+  property("empty") = forAll { a: Int =>
+    val h = insert(a, empty)
+    deleteMin(h) == empty
+  }
+
+  property("rightmin") = forAll { (a: Int, b: Int) =>
+    val min = Math.min(a, b)
+    min == findMin(insert(a, insert(b, empty)))
+  }
+
   property("assoc1") = forAll { (a: Int, b: Int) =>
-  	insert(a, insert(b, empty)) == insert(b, insert(a, empty))
+    insert(a, insert(b, empty)) == insert(b, insert(a, empty))
   }
 
   property("assoc2") = forAll { (a: H, b: H, c: H) =>
@@ -24,6 +34,27 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
 
   property("assoc3") = forAll { (a: Int, b: Int) =>
     meld(insert(a, empty), insert(b, empty)) == insert(a, meld(empty, insert(b, empty)))
+  }
+
+  property("ordered") = forAll { (a: H) =>
+    def isOrdered(previous: Int, ts: H):Boolean = { 
+      if (isEmpty(ts))
+        true
+      else {
+        val min = findMin(ts)
+        if (previous > min) false
+        else isOrdered(min, deleteMin(ts))
+      }
+    }
+    val min = findMin(a)
+    isOrdered(min, deleteMin(a))
+  }
+
+  property("min2heaps") = forAll { (a: H, b: H) =>
+    val mina = findMin(a)
+    val minb = findMin(b)
+    val expected = if (mina < minb) mina else minb
+    expected == findMin(meld(a, b))
   }
 
   lazy val genHeap: Gen[H] = for {
