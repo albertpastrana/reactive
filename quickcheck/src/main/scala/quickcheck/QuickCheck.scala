eb4c454d8e4ba099b6e24285009ba57b6740e647
Albert Pastrana <albert.pastrana@gmail.com>
2013-11-11 20:20:19 +0000

    Implemented equals, all green.
    Instead of comparing heaps only by using findMin once,
    I've implemented an equals method that compares it
    recursively.

--- quickcheck/src/main/scala/quickcheck/QuickCheck.scala
+++ quickcheck/src/main/scala/quickcheck/QuickCheck.scala
@@ -30,7 +30,7 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
   }
 
   property("commut") = forAll { (a: H, b: H) =>
-    findMin(meld(a, b)) == findMin(meld(b, a))
+    equals(meld(a, b), meld(b, a))
   }
 
   property("assoc1") = forAll { (a: Int, b: Int) =>
@@ -38,11 +38,11 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
   }
 
   property("assoc2") = forAll { (a: H, b: H, c: H) =>
-    findMin(meld(meld(a, b), c)) == findMin(meld(a, meld(b, c)))
+    equals(meld(meld(a, b), c), (meld(a, meld(b, c))))
   }
 
   property("assoc3") = forAll { (a: Int, b: Int) =>
-    meld(insert(a, empty), insert(b, empty)) == insert(a, meld(empty, insert(b, empty)))
+    equals(meld(insert(a, empty), insert(b, empty)), insert(a, meld(empty, insert(b, empty))))
   }
 
   //Given any heap, you should get a sorted sequence of elements when continually finding
@@ -73,8 +73,17 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
   property("min2heaps") = forAll { (a: H, b: H) =>
     val mina = findMin(a)
     val minb = findMin(b)
-    val expected = if (mina < minb) mina else minb
+    val expected = Math.min(mina, minb)
     expected == findMin(meld(a, b))
+  }
+
+  //Finding a minimum of the melding of any 3 heaps should return a minimum of one or the other.
+  property("min3heaps") = forAll { (a: H, b: H, c: H) =>
+    val mina = findMin(a)
+    val minb = findMin(b)
+    val minc = findMin(c)
+    val expected = Math.min(Math.min(mina, minb), minc)
+    expected == findMin(meld(meld(a, b), c))
   }
 
   property("size2->empty") = forAll { (a: Int) =>
@@ -82,9 +91,24 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
     deleteMin(deleteMin(h)) == empty
   }
 
+  def equals(a: H, b: H):Boolean = { 
+    if (isEmpty(a) && isEmpty(b))
+      true
+    else if (isEmpty(a) && !isEmpty(b))
+      false
+    else if (!isEmpty(a) && isEmpty(b))
+      false
+    else {
+      val mina = findMin(a)
+      val minb = findMin(b)
+      if (mina != minb) false
+      else equals(deleteMin(a), deleteMin(b))
+    }
+  }
+
   lazy val genHeap: Gen[H] = for {
   	k <- arbitrary[Int]
-  	m <- oneOf(empty, genHeap)
+  	m <- oneOf(empty, genHeap, genHeap, genHeap, genHeap, genHeap, genHeap, genHeap, genHeap)
   } yield insert(k, m)
 
   implicit lazy val arbHeap: Arbitrary[H] = Arbitrary(genHeap)
